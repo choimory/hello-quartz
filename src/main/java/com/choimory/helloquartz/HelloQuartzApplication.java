7257f84e56ca6615f64376d3d1ee225d8b6cf7d8
choimory <choimory@illunex.com>
2021-06-30 18:16:44 +0900

    Initial commit

---
+++ src/main/java/com/choimory/helloquartz/HelloQuartzApplication.java
@@ -0,0 +1,13 @@
+package com.choimory.helloquartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloQuartzApplication.class, args);
+    }
+
+}
